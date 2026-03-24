5bdaafbd4882bf49898fc08db060f4e8f6a3db1c
mviersel <119597405+mviersel@users.noreply.github.com>
2026-03-24 16:40:47 +0100

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,124 +1,45 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+add_dock_app() {
+  local app_path="$1"
+
+  defaults write com.apple.dock persistent-apps -array-add \
+    "<dict>
+      <key>tile-data</key>
+      <dict>
+        <key>file-data</key>
+        <dict>
+          <key>_CFURLString</key>
+          <string>${app_path}</string>
+          <key>_CFURLStringType</key>
+          <integer>0</integer>
+        </dict>
+      </dict>
+    </dict>"
+}
+
 echo "Configuring Dock..."
 
-# Auto-hide Dock
 defaults write com.apple.dock autohide -bool true
-
-# No recent applications
 defaults write com.apple.dock show-recents -bool false
-
-# Small Dock size
-defaults write com.apple.dock tilesize -int 32
-
-# Align Dock to the right
+defaults write com.apple.dock tilesize -int 50
 defaults write com.apple.dock orientation -string right
 
-# Empty the Dock
 defaults write com.apple.dock persistent-apps -array
 defaults write com.apple.dock persistent-others -array
 
-# Add apps back in the order you want
-defaults write com.apple.dock persistent-apps -array-add \
-  '<dict>
-    <key>tile-data</key>
-    <dict>
-      <key>file-data</key>
-      <dict>
-        <key>_CFURLString</key>
-        <string>/System/Library/CoreServices/Finder.app</string>
-        <key>_CFURLStringType</key>
-        <integer>0</integer>
-      </dict>
-    </dict>
-  </dict>'
+add_dock_app "/System/Applications/Apps.app"
 
-defaults write com.apple.dock persistent-apps -array-add \
-  '<dict>
-    <key>tile-data</key>
-    <dict>
-      <key>file-data</key>
-      <dict>
-        <key>_CFURLString</key>
-        <string>/System/Applications/System Settings.app</string>
-        <key>_CFURLStringType</key>
-        <integer>0</integer>
-      </dict>
-    </dict>
-  </dict>'
+# Alleen toevoegen als Launchpad nog bestaat op deze macOS-versie
+if [[ -d "/System/Applications/Launchpad.app" ]]; then
+  add_dock_app "/System/Applications/Launchpad.app"
+fi
 
-defaults write com.apple.dock persistent-apps -array-add \
-  '<dict>
-    <key>tile-data</key>
-    <dict>
-      <key>file-data</key>
-      <dict>
-        <key>_CFURLString</key>
-        <string>/System/Applications/Launchpad.app</string>
-        <key>_CFURLStringType</key>
-        <integer>0</integer>
-      </dict>
-    </dict>
-  </dict>'
+add_dock_app "/Applications/Zen.app"
+add_dock_app "/Applications/Obsidian.app"
+add_dock_app "/Applications/Microsoft Teams.app"
+add_dock_app "/Applications/Microsoft Outlook.app"
 
-defaults write com.apple.dock persistent-apps -array-add \
-  '<dict>
-    <key>tile-data</key>
-    <dict>
-      <key>file-data</key>
-      <dict>
-        <key>_CFURLString</key>
-        <string>/Applications/Zen.app</string>
-        <key>_CFURLStringType</key>
-        <integer>0</integer>
-      </dict>
-    </dict>
-  </dict>'
-
-defaults write com.apple.dock persistent-apps -array-add \
-  '<dict>
-    <key>tile-data</key>
-    <dict>
-      <key>file-data</key>
-      <dict>
-        <key>_CFURLString</key>
-        <string>/Applications/Obsidian.app</string>
-        <key>_CFURLStringType</key>
-        <integer>0</integer>
-      </dict>
-    </dict>
-  </dict>'
-
-defaults write com.apple.dock persistent-apps -array-add \
-  '<dict>
-    <key>tile-data</key>
-    <dict>
-      <key>file-data</key>
-      <dict>
-        <key>_CFURLString</key>
-        <string>/Applications/Microsoft Teams.app</string>
-        <key>_CFURLStringType</key>
-        <integer>0</integer>
-      </dict>
-    </dict>
-  </dict>'
-
-defaults write com.apple.dock persistent-apps -array-add \
-  '<dict>
-    <key>tile-data</key>
-    <dict>
-      <key>file-data</key>
-      <dict>
-        <key>_CFURLString</key>
-        <string>/Applications/Microsoft Outlook.app</string>
-        <key>_CFURLStringType</key>
-        <integer>0</integer>
-      </dict>
-    </dict>
-  </dict>'
-
-# Restart Dock
-killall Dock
-
+killall Dock || true
 echo "Dock configured."
