22e107d1b0147395de6fb805be39246d572bdba1
mviersel <119597405+mviersel@users.noreply.github.com>
2026-03-25 14:32:34 +0100

    Add aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,8 +1,15 @@
-cl() {
-  printf "\033c"
-  fastfetch
-}
+alias cl="print '\033c' && fastfetch"
+alias brundle="brew bundle --file ~/mac-dotfiles/brewfiles/brewfile "
 
-brundle() {
-  brew bundle --file ~/mac-dotfiles/brewfiles/brewfile
+alias la="ls -la"
+
+# git 
+alias gs="git status"
+alias gp="git pull"
+alias gc="git commit"
+
+alias v="nvim"
+
+mkcd() {
+  mkdir -p "$1" && cd "$1"
 }
