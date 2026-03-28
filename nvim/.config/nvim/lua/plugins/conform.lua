2f642f065e7f97d1fe516685d32b68fa6259f024
mviersel <119597405+mviersel@users.noreply.github.com>
2026-03-28 20:52:05 +0100

    Update conform.lua

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      lua = { "stylua" },
       javascript = { "prettier" },
       typescript = { "prettier" },
       javascriptreact = { "prettier" },
