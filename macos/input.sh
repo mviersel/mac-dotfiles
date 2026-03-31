35a44a9bb1944f4a03ecc01d6c6e0457209c11cb
mviersel <119597405+mviersel@users.noreply.github.com>
2026-03-31 15:37:46 +0200

    Update input.sh

--- macos/input.sh
+++ macos/input.sh
@@ -4,5 +4,10 @@ set -euo pipefail
 echo "Configuring input..."
 
 defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls -bool true
+defaults write com.apple.BezelServices kDimTime -int 1
+
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
 
 echo "Input configured ✅"
