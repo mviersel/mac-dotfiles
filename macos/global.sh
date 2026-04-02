c7449aa01933ec025d82aa285f17af712d2a82ce
mviersel <119597405+mviersel@users.noreply.github.com>
2026-04-02 09:04:51 +0200

    Update standard wallpaper

--- macos/global.sh
+++ macos/global.sh
@@ -1,1 +1,5 @@
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+
+WALLPAPER="$HOME/.config/backgrounds/greenComputer.jpeg"
+
+osascript -e "tell application \"System Events\" to set picture of every desktop to \"$WALLPAPER\""
