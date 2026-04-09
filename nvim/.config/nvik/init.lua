f32bcc7e38ba983132fab38a7f6192cc0d4c8ad6
mviersel <119597405+mviersel@users.noreply.github.com>
2026-04-09 09:26:51 +0200

    add highlight plugin

--- nvim/.config/nvik/init.lua
+++ nvim/.config/nvik/init.lua
@@ -68,6 +68,17 @@ require("lazy").setup({
 			vim.lsp.enable("jdtls")
 		end,
 	},
+
+	{
+		"brenoprata10/nvim-highlight-colors",
+		opts = {
+			render = "virtual", -- of "background" / "foreground"
+			virtual_symbol = "■",
+			virtual_symbol_position = "inline", -- of "eol"
+			enable_named_colors = true,
+			enable_tailwind = true,
+		},
+	},
 })
 
 -- ========== KEYMAPS =========
---
+++ nvim/.config/nvim/lua/plugins/highlight.lua
@@ -0,0 +1,10 @@
+return {
+  "brenoprata10/nvim-highlight-colors",
+  opts = {
+    render = "virtual", -- of "background" / "foreground"
+    virtual_symbol = "■",
+    virtual_symbol_position = "inline", -- of "eol"
+    enable_named_colors = true,
+    enable_tailwind = true,
+  },
+}
