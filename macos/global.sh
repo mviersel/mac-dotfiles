04c634276d2394fc18c66b2b100d72cb27630f4d
mviersel <119597405+mviersel@users.noreply.github.com>
2026-04-18 15:16:55 +0200

    set menu bar to hidden and less transparency

--- macos/global.sh
+++ macos/global.sh
@@ -3,3 +3,8 @@ defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 WALLPAPER="$HOME/.config/backgrounds/greenComputer.jpeg"
 
 osascript -e "tell application \"System Events\" to set picture of every desktop to \"$WALLPAPER\""
+
+defaults write .GlobalPreferences AppleMenuBarVisibleInFullscreen -bool false
+defaults write .GlobalPreferences _HIHideMenuBar -bool true
+
+defaults write com.apple.Accessibility EnhancedBackgroundContrastEnabled -bool true
