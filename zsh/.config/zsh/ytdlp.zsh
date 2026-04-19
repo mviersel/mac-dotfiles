dee34480aad6a84e5316fd70ce283b8602ba52a1
mviersel <119597405+mviersel@users.noreply.github.com>
2026-04-19 18:24:56 +0200

    Add mp3

--- zsh/.config/zsh/ytdlp.zsh
+++ zsh/.config/zsh/ytdlp.zsh
@@ -1,3 +1,12 @@
 function ythd() {
     yt-dlp -f "bv*[height=1080]+ba/b[height=1080]" --merge-output-format mp4 "$@"
   }
+
+ytmp3() {
+  if [ $# -eq 0 ]; then
+    echo "Usage: ytmp3 <url>"
+    return 1
+  fi
+
+  yt-dlp -x --audio-format mp3 --audio-quality 0 "$@"
+}
