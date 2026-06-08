99c909db2cab796b7645011c15309be76b4ff27b
mviersel <119597405+mviersel@users.noreply.github.com>
2026-06-08 14:14:45 +0200

    add thumbnail to ytdlp script

--- zsh/.config/zsh/ytdlp.zsh
+++ zsh/.config/zsh/ytdlp.zsh
@@ -8,5 +8,13 @@ ytmp3() {
     return 1
   fi
 
-  yt-dlp -x --audio-format mp3 --audio-quality 0 "$@"
+  yt-dlp \
+    --ignore-errors \
+    -x \
+    --audio-format mp3 \
+    --audio-quality 0 \
+    --embed-thumbnail \
+    --convert-thumbnails jpg \
+    --add-metadata \
+    "$@"
 }
