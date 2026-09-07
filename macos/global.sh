6501ab01318b0ec486c9db22fb1ba83dafa9bd72
mviersel <119597405+mviersel@users.noreply.github.com>
2026-09-07 09:27:47 +0200

    turn off mouse accel in dotfiles

--- macos/global.sh
+++ macos/global.sh
@@ -6,5 +6,6 @@ osascript -e "tell application \"System Events\" to set picture of every desktop
 
 defaults write .GlobalPreferences AppleMenuBarVisibleInFullscreen -bool false
 defaults write .GlobalPreferences _HIHideMenuBar -bool true
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
 
 defaults write com.apple.Accessibility EnhancedBackgroundContrastEnabled -bool true
