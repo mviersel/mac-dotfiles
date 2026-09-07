4f4bb210d1b34e56dcdb0bb29274ee16be30eac7
mviersel <119597405+mviersel@users.noreply.github.com>
2026-09-07 22:41:58 +0200

    add ability for keyboard backlight to turn off after 30s

--- macos/global.sh
+++ macos/global.sh
@@ -4,8 +4,12 @@ WALLPAPER="$HOME/.config/backgrounds/greenComputer.jpeg"
 
 osascript -e "tell application \"System Events\" to set picture of every desktop to \"$WALLPAPER\""
 
+# auto hide menubar so the sketchybar has space
 defaults write .GlobalPreferences AppleMenuBarVisibleInFullscreen -bool false
 defaults write .GlobalPreferences _HIHideMenuBar -bool true
+# turn off mouse acceleration
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
+# Turn keyboard backlight off after 30 seconds of inactivity
+defaults write com.apple.BezelServices kDimTime -int 30
 
 defaults write com.apple.Accessibility EnhancedBackgroundContrastEnabled -bool true
