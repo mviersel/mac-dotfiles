f3e4f39cf154b26bb0b50a6ed9d0172a1ef1cf16
mviersel <119597405+mviersel@users.noreply.github.com>
2026-09-16 11:53:05 +0200

    change headphone icons

--- sketchybar/.config/sketchybar/plugins/volume.sh
+++ sketchybar/.config/sketchybar/plugins/volume.sh
@@ -6,11 +6,30 @@ if [ -z "$VOLUME" ]; then
   VOLUME="$(osascript -e 'output volume of (get volume settings)' 2>/dev/null)"
 fi
 
-case "$VOLUME" in
-  [6-9][0-9]|100) ICON="󰕾" ;;
-  [3-5][0-9]) ICON="󰖀" ;;
-  [1-9]|[1-2][0-9]) ICON="󰕿" ;;
-  *) ICON="󰖁" ;;
+OUTPUT_DEVICE="$(system_profiler SPAudioDataType 2>/dev/null | awk '
+  /^        [^ ]/ {
+    device = $0
+    sub(/^        /, "", device)
+    sub(/:$/, "", device)
+  }
+  /Default Output Device: Yes/ {
+    print device
+    exit
+  }
+')"
+
+case "$OUTPUT_DEVICE" in
+  *AirPods*|*Headphone*|*Headset*|*Buds*|*Earphone*|*WH-*|*WF-*|*Beats*|*QuietComfort*|*XM[0-9]*)
+    ICON="󰋋"
+    ;;
+  *)
+    case "$VOLUME" in
+      [6-9][0-9]|100) ICON="󰕾" ;;
+      [3-5][0-9]) ICON="󰖀" ;;
+      [1-9]|[1-2][0-9]) ICON="󰕿" ;;
+      *) ICON="󰖁" ;;
+    esac
+    ;;
 esac
 
 sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
