b0257c83007bdb66c29b23b8ea34d418930cb6fb
mviersel <119597405+mviersel@users.noreply.github.com>
2026-09-16 11:20:14 +0200

    change text and icons to white

--- sketchybar/.config/sketchybar/plugins/aerospace.sh
+++ sketchybar/.config/sketchybar/plugins/aerospace.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-ACCENT_COLOR=0xffb8d4b8
+ACCENT_COLOR=0xffffffff
 TEXT_COLOR=0xfff5f5f5
-MUTED_COLOR=0x99f5f5f5
+MUTED_COLOR=0xffffffff
 WORKSPACE="$1"
 FOCUSED="${FOCUSED_WORKSPACE:-$(aerospace list-workspaces --focused)}"
 
 if [ "$WORKSPACE" = "$FOCUSED" ]; then
-  sketchybar --set "$NAME" icon="●" icon.color="$ACCENT_COLOR"
+    sketchybar --set "$NAME" icon="●" icon.color="$ACCENT_COLOR"
 else
-  sketchybar --set "$NAME" icon="○" icon.color="$MUTED_COLOR"
+    sketchybar --set "$NAME" icon="○" icon.color="$MUTED_COLOR"
 fi
